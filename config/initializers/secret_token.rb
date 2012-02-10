3e34f7894fbf227f5682e6544d3aceb343ae7c6b
swathihk <hk.swathi56@gmail.com>
2012-02-11 01:14:53 +0530

    for testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gurukuldeveloper::Application.config.secret_token = 'cc1e06c5c3fb2d402c3b40023c74694f207cd1f0bbedd86c28e88b0d9d80c3be8a3c720e5d68a0af20deec41c18f340a3efd106b1b9913318556ec973affbf0b'
